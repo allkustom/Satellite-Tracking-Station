8fe8233e573ea8052614fdbd29671016813bff4d
MinkyuKim97 <allkustom97@gmail.com>
2026-02-26 13:30:48 -0500

    Feb 26th_Coord point to point

--- 00 Arduino/00 Test/01WristDifferential_withNema17/01WristDifferential_withNema17.ino
+++ 00 Arduino/00 Test/01WristDifferential_withNema17/01WristDifferential_withNema17.ino
@@ -18,7 +18,7 @@ const int fullStepRev = 200;
 // 1/16 step -> 16
 const int microStepping = 16;
 const int stepsPerRevolution = fullStepRev * microStepping;
-const float speedAmp = 5.0f;
+const float speedAmp = 2.0f;
 
 // Higher number = faster
 const int speeds[2] = {20 * microStepping * speedAmp, 200 * microStepping * speedAmp};
@@ -30,15 +30,10 @@ AccelStepper* st[stepperCount];
 
 String msg;
 bool moving = false;
+int cmdMsgCount = 0;
 
 // Angle calculation with Trig
 const float RAD2DEG = 180.0 / M_PI;
-float angle_1;
-float angle_2;
-float rev_1;
-float rev_2;
-bool dir_yaw = true;
-bool dir_pitch = true;
 
 struct Coord {
   float x,y,z;
@@ -49,9 +44,25 @@ float maxRadius = 100.0f;
 Coord CurrentCoord = {0.0, maxRadius, 0.0 };
 Coord TargetCoord = {0.0, 0.0, 0.0 };
 
+//---------------------------------------------------------
+//---------------------------------------------------------
+// Utilites
 long revToSteps(float rev){
   return lround(rev * (float)stepsPerRevolution);
 }
+
+float wrapDeg(float a){
+  while(a > 180.0f) a -= 360.0f;
+  while(a <= -180.0f) a += 360.0f;
+  return a;
+}
+
+
+
+
+//---------------------------------------------------------
+//---------------------------------------------------------
+// Motor Settings
 
 // Enable/Disable motor
 void enableMotor(bool activate){
@@ -73,50 +84,52 @@ void setProfileMotorSep(int index, float maxSpeed, float accelSpeeds){
   Serial.println("Motor: " + (String)index + " - Speed: " + (String)maxSpeed + "/Accel: " + (String)accelSpeeds);
 }
 
-int dirSet(bool dir, bool sep, int index){
-  int base = dir ? -1 : +1;
-  if(!sep) return base;
+//---------------------------------------------------------
+//---------------------------------------------------------
 
-  if(index == 0) return base;
-  if(index == 1) return -base;
-  return base;
-}
+// Controling both motor individually
+void stepperRot(float rev_L, float rev_R, int speed){
+  // Set direction of rotation
+  // [true] => clockwise
+  // [false] =? counter-clockwise
+  int dirL = rev_L < 0 ? -1 : 1;
+  int dirR = rev_R < 0 ? -1 : 1;
 
-void stepperRot(bool dir, bool sep, float rev_L, float rev_R, int speed){
-  long moveSteps_L = labs(revToSteps(rev_L));
-  long moveSteps_R = labs(revToSteps(rev_R));
-  if(moveSteps_L < 0 || moveSteps_R < 0) return;
+  // Turn into long and apply absolute
+  double moveSteps_L = labs(revToSteps(rev_L));
+  double moveSteps_R = labs(revToSteps(rev_R));
 
-  // enableMotor(true);
-  // setProfileMotor(speed, accelSpeed);
 
-  // Num. 0 => Left
-  // Num. 1 => Right
-
-  if(rev_L > rev_R){
-    float adjustSpeed = rev_L/rev_R*speed ;
-    float adjustAccel = rev_L/rev_R*accelSpeed ;
-    setProfileMotorSep(0,adjustSpeed, adjustAccel);
-    setProfileMotorSep(1,speed, accelSpeed);
-  }else if(rev_L < rev_R){
-    int adjustSpeed = rev_R/rev_L * speed ;
-    float adjustAccel = rev_R/rev_L * accelSpeed ;
-    setProfileMotorSep(1, adjustSpeed, adjustAccel);
+  // Map speed, accel
+  // When the goal revolution is different,
+  // slower the less revoluting moter
+  float adjustSpeed;
+  float adjustAccel;
+  if(moveSteps_L > moveSteps_R){
+    adjustSpeed = moveSteps_R/moveSteps_L * speed ;
+    adjustAccel = moveSteps_R/moveSteps_L * accelSpeed ;
     setProfileMotorSep(0,speed, accelSpeed);
+    setProfileMotorSep(1,adjustSpeed, adjustAccel);
+  }else if(moveSteps_L < moveSteps_R){
+    adjustSpeed = moveSteps_L/moveSteps_R * speed ;
+    adjustAccel = moveSteps_L/moveSteps_R * accelSpeed ;
+    setProfileMotorSep(0, adjustSpeed, adjustAccel);
+    setProfileMotorSep(1,speed, accelSpeed);
   }else{
     setProfileMotor(speed, accelSpeed);
   }
 
-  for(int i =0; i< stepperCount; i++){
-    int dSet = dirSet(dir, sep, i);
-    if(i == 0){
-      long target = st[i]->currentPosition() + (long)dSet * moveSteps_L;
-      st[i]->moveTo(target);
-    }else{
-      long target = st[i]->currentPosition() + (long)dSet * moveSteps_R;
-      st[i]->moveTo(target);
-    }
-  }
+  // Set target and direction
+  long target;
+  // target = st[0]->currentPosition() + moveSteps_L;
+  // st[0]->moveTo(target);
+  // target = st[1]->currentPosition() + moveSteps_R;
+  // st[1]->moveTo(target);
+  target = st[0]->currentPosition() + (long)dirL * moveSteps_L;
+  st[0]->moveTo(target);
+  target = st[1]->currentPosition() + (long)dirR * moveSteps_R;
+  st[1]->moveTo(target);
+
 
   bool anyRunning;
   do{
@@ -129,161 +142,132 @@ void stepperRot(bool dir, bool sep, float rev_L, float rev_R, int speed){
     }
   }
   while (anyRunning);
-
 }
 
-void actionSeq(int cases){
-  switch(cases){
-    case 0:
-      stepperRot(true, false, 0.5f,0.5f, speeds[0]);
-      break;
-    case 1:
-      stepperRot(false, false, 0.5f,0.5f, speeds[0]);
-      break;
-    case 2:
-      stepperRot(true, true, 0.25f,0.25f, speeds[0]);
-      break;
-    case 3:
-      stepperRot(false, true, 0.25f,0.25f, speeds[0]);
-      break;
-    case 4:
-      stepperRot(true, false, 0.5f,0.25f, speeds[0]);
-      break;
-    case 5:
-      stepperRot(false, false, 0.5f,0.25f, speeds[0]);
-      break;
-
-    // Coordinate tracking
-    case 6:
-      // angleCal(120.0, 50.0, 100.0);
-      angleCal(10.0, 10.0, 10.0);
-      
-      break;
-
-    case 7:
-      // angleCal(-120.0, 50.0, 20.0);
-      angleCal(-15.0, -5.0, 5.0);
-      
-
-      break;
-      
-
-    // Action Seq
-    case 9:
-      angleCal(0,maxRadius, 0);
-
-            // stepperRot(true, true, 0.25f, 0.25f, speeds[0]);
-      // stepperRot(false, true, 0.5f, 0.5f, speeds[1]);
-      // stepperRot(true, true, 0.25f, 0.25f, speeds[1]);
-      // stepperRot(true, false, 0.5f, 0.25f, speeds[0]);
-      // stepperRot(false, false, 0.5f, 0.25f, speeds[0]);
-      // stepperRot(true, false, 0.5f, 0.5f, speeds[0]);
-      // stepperRot(false, false, 0.5f, 0.5f, speeds[1]);
-      break;
+// void actionSeq(int cases){
+//   switch(cases){
+//     // Coordinate tracking
+//     case 6:
+//       angleCal(10.0, 10.0, 10.0);      
+//       break;
+//     case 7:
+//       angleCal(-15.0, -5.0, 5.0);
+//       break;
+//     case 9:
+//       angleCal(0,maxRadius, 0);
+//       break;
   
-    default:
-      Serial.println("CMD out fo range");
-      moving = false;
-      break;
+//     default:
+//       Serial.println("CMD out fo range");
+//       moving = false;
+//       break;
+//   }
+// }
 
+void cmdCoord(int cmd){
+  if(cmd == 9999 ){
+    cmdMsgCount = 0;
+    Serial.println("Reset coord cmd");
+    return;    
   }
+  
+  if(cmdMsgCount == 0 ){
+    TargetCoord.x = cmd;
+    Serial.print("Target X: ");
+    Serial.println(cmd);
+  }else if(cmdMsgCount == 1 ){
+    TargetCoord.y = cmd;
+    Serial.print("Target Y: ");
+    Serial.println(cmd);
+  }else{
+    TargetCoord.z = cmd;
+    Serial.print("Target Z: ");
+    Serial.println(cmd);
+
+    cmdMsgCount = 0;
+    moving = true;
+
+    Serial.print("Target Coordinate: ");
+    Serial.print(TargetCoord.x);
+    Serial.print(" / ");
+    Serial.print(TargetCoord.y);
+    Serial.print(" / ");
+    Serial.println(TargetCoord.z);
+    angleCal();
+    moving = false;
+    return;
+  }
+  cmdMsgCount++;
 }
 
-static inline float wrapDeg(float a){
-  while(a > 180.0f) a -= 360.0f;
-  while(a <= -180.0f) a += 360.0f;
-  return a;
-}
 
-void angleCal(float coordX, float coordY, float coordZ){
-  TargetCoord = {coordX,coordY,coordZ};
 
-  // float normX = fabsf(coordX);
-  // float normY = fabsf(coordY);
-  // float normZ = fabsf(coordZ);
-  // Serial.println("Norm_1 :" + (String)normX + " / "+ (String)normY + " / "+ (String)normZ + " / ");
+void angleCal(){
+  // TargetCoord = {coordX,coordY,coordZ};
 
-  // float target_cal_1 = normY / hypot(normX,normY);
-  // float target_cal_2 = normZ / (sqrt(pow(normX,2) + pow(normY,2) + pow(normZ,2)));
+  float azim_t = atan2(TargetCoord.y, TargetCoord.x) * RAD2DEG;
+  float hori_t = hypot(TargetCoord.x, TargetCoord.y);
+  float elev_t = atan2(TargetCoord.z, hori_t) * RAD2DEG;
 
-  // normX = fabsf(CurrentCoord.x);
-  // normY = fabsf(CurrentCoord.y);
-  // normZ = fabsf(CurrentCoord.z);
-
-  // float current_cal_1 = normY / (sqrt(pow(normX,2) + pow(normY, 2)));
-  // float current_cal_2 = normZ / (sqrt(pow(normX,2) + pow(normY,2) + pow(normZ,2)));
-  // if(normX == 0.0 && normY == 0.0 && normZ == 0.0){
-  //   angle_1 = (acos(target_cal_1) * RAD2DEG);
-  //   angle_2 = (acos(target_cal_2) * RAD2DEG);
-  // }else{
-  //   angle_1 = (acos(target_cal_1) * RAD2DEG) + (acos(current_cal_1) * RAD2DEG);
-  //   angle_2 = (acos(target_cal_2) * RAD2DEG) - (acos(current_cal_2) * RAD2DEG);
-  // }
-
-  //-----------------------------------------------
-  //-----------------------------------------------
-  // Feb 24th
-  // Made a mistake
-  // It was better to use tan instead of cos
-  // shift the logic into 'atan2' to check the coordinate's quadrant
-
-  float yaw_t = atan2(coordY, coordX) * RAD2DEG;
-  float hori_t = hypot(coordX, coordY);
-  float pitch_t = atan2(coordZ, hori_t) * RAD2DEG;
-
-  float yaw_c = atan2(CurrentCoord.y, CurrentCoord.x) * RAD2DEG;
+  float azim_c = atan2(CurrentCoord.y, CurrentCoord.x) * RAD2DEG;
   float hori_c = hypot(CurrentCoord.x, CurrentCoord.y);
-  float pitch_c = atan2(CurrentCoord.z, hori_c) * RAD2DEG;
+  float elev_c = atan2(CurrentCoord.z, hori_c) * RAD2DEG;
 
-  angle_1 = wrapDeg(yaw_t - yaw_c);
-  angle_2 = pitch_t - pitch_c;
+  float angle_1 = wrapDeg(azim_t - azim_c);
+  float angle_2 = elev_t - elev_c;
 
 
   // Use result as a direction
-  dir_yaw = angle_1 < 0 ? true : false;
-  dir_pitch = angle_2 < 0 ? false : true;
+  bool dir_azim = angle_1 < 0 ? true : false;
+  bool dir_elev = angle_2 < 0 ? false : true;
 
-  // Serial.println("Angle_1: " + (String)angle_1 + " / dir_yaw: " + dir_yaw);
-  // Serial.println("Angle_2: " + (String)angle_2 + " / dir_pitch: " + dir_pitch);
-
-
-
+  // Serial.println("Angle_1: " + (String)angle_1 + " / dir_azim: " + dir_azim);
+  // Serial.println("Angle_2: " + (String)angle_2 + " / dir_elev: " + dir_elev);
 
   angle_1 = fabsf(angle_1);
   angle_2 = fabsf(angle_2);
+  float rev_1 = angle_1 / 360;
+  float rev_2 = angle_2 / 360;
 
-  updateCurrentRev();
-  updateCurrentCoord(coordX, coordY, coordZ);
-  rotateToTarget();
+  rotToTarget(dir_azim, rev_1, dir_elev, rev_2);
+  updateCurrentCoord(TargetCoord);
 }
 
-void rotateToTarget(){
+void rotToTarget(bool dir_azim, float rev_1, bool dir_elev, float rev_2){
 
-  // For debugging purpose, separated the 2 angle movement
-  // Calculate right result based on the direction and turn it into a point to point smooth action
-  stepperRot(dir_yaw, false, rev_1, rev_1, speeds[0]);
-  stepperRot(dir_pitch, true, rev_2, rev_2, speeds[0]);
+  float rev_1_L = rev_1;
+  float rev_1_R = rev_1;
+  float rev_2_L = rev_2;
+  float rev_2_R = rev_2;
 
-  // if(rev_1 < rev_2){
-  //   stepperRot(dir_pitch,true, rev_1+rev_2, rev_2 - rev_1, speeds[0]);
-  // }else{
-  //   stepperRot(dir_yaw,false, rev_1+rev_2, rev_1 - rev_2, speeds[0]);
-  // }
+  if(dir_azim){
+    rev_1_L = rev_1_L * -1;
+    rev_1_R = rev_1_R * -1;
+  }
+  if(dir_elev){
+    rev_2_L = rev_2_L * -1;
+  }else{
+    rev_2_R = rev_2_R * -1;
+  }
+
+  float rev_L = rev_1_L + rev_2_L;
+  float rev_R = rev_1_R + rev_2_R;
+  
+  Serial.println("rev_1: " + (String)rev_1 + " / " + "rev_2: " + (String)rev_2);
+  Serial.println("rev_L: " + (String)rev_L + " / " + "rev_R: " + (String)rev_R);
+
+  stepperRot(rev_L, rev_R, speeds[0]);
+
 }
 
-void updateCurrentRev(){
-  rev_1 = angle_1 / 360;
-  rev_2 = angle_2 / 360;
-  Serial.println("rev_1 : " + (String)rev_1);
-  Serial.println("rev_2 : " + (String)rev_2);
-    
-}
-void updateCurrentCoord(float coordX, float coordY, float coordZ){
-  CurrentCoord.x = coordX;
-  CurrentCoord.y = coordY;
-  CurrentCoord.z = coordZ;
-}
 
+void updateCurrentCoord(Coord v){
+  CurrentCoord.x = v.x;
+  CurrentCoord.y = v.y;
+  CurrentCoord.z = v.z;
+}
+//---------------------------------------------------------
+//---------------------------------------------------------
 
 void setup() {
   Serial.begin(9600);
@@ -296,6 +280,7 @@ void setup() {
       digitalWrite(enPin[i], LOW);
     }
   }
+  
   pinMode(stepControlPin, OUTPUT);
   digitalWrite(stepControlPin, HIGH);
 
@@ -305,19 +290,10 @@ void setup() {
     st[i]->setMinPulseWidth(3);
     st[i]->setMaxSpeed(speeds[0]);
     st[i]->setAcceleration(accelSpeed);
-
   }  
 
-  // This for changing the step mode
-  // pinMode(ms1, OUTPUT);
-  // pinMode(ms2, OUTPUT);
-  // pinMode(ms3, OUTPUT);
-  // digitalWrite(ms1,HIGH);
-  // digitalWrite(ms2,HIGH);
-
-  delay(200);
-
-    Serial.println("CMD Ready");
+  delay(1000);
+  Serial.println("CMD Ready");
 
 
 
@@ -333,14 +309,17 @@ void loop() {
         int cmd = msg.toInt();
         msg = "";
 
-        Serial.print("CMD = ");
-        Serial.println(cmd);
+        cmdCoord(cmd);
+        // Serial.print("CMD = ");
+        // Serial.println(cmd);
 
-        actionSeq(cmd);
-        moving = true;
+        // actionSeq(cmd);
+        // moving = true;
       }
     } else {
-      msg += c;
+      if(!moving){
+        msg += c;
+      }
     }
   }
 
